79c6ea86dfd9c910db8318f0cb8205b81e227e83
Thomas Logan <1215556+tlogan@users.noreply.github.com>
2025-07-28 22:13:29 -0700

    update rules with negative position restriction

--- dsn/Lang/Dynamic.lean
+++ dsn/Lang/Dynamic.lean
@@ -141,22 +141,94 @@ inductive Progression : Expr → Expr → Prop
 | loopinflate : ∀ {id e},
   Progression (.loop (.function [(.var id, e)])) (sub [(id, (.loop (.function [(.var id, e)])))] e)
 
+inductive Multi : Expr → Expr → Prop
+| refl {e} : Multi e e
+| step {e e' e''} : Progression e e' → Multi e e'' → Multi e e''
 
+-- TODO: separate out restricted version of model typing used in negations
 
 mutual
-
   inductive Dynamic.Typing : List (String × Typ) → Expr → Typ → Prop
   | expan : ∀ {δ e τ e'},
     Progression e e' → Typing δ e' τ → Typing δ e τ
   | record : ∀ {δ r l τ v},
     IsValue (.record r) → (l, v) ∈ r → Typing δ v τ →
     Typing δ (.record r) (.entry l τ)
-  | funhead : ∀ {δ p e f left right},
-    -- (∀ σ, Typing δ (sub σ (Pat.toExpr p)) left → Typing δ (sub σ e) right) →
-    Typing δ (.function ((p,e)::f)) (.path left right)
+  -- | funhead : ∀ {δ v p e f left right},
+  --   (∀ {v}, Typing δ v left → ∃ σ, pattern_match v p = some σ ∧ Typing δ (sub σ e) right) →
+  --   Typing δ (.function ((p,e)::f)) (.path left right)
 end
 
 
--- TODO: define progression and dynamic typing
--- TODO: create tactic to construct proof of progression to a value
--- TODO: create tactic to construct proof of dynamic values for given expression and type
+---------------------------------------------------------------
+------ Experimental --------------------------------------------
+---------------------------------------------------------------
+
+inductive MyAcc.{u} {α : Sort u} (r : α -> α -> Prop) : α -> Prop
+| intro (x : α): (∀ y, r y x -> MyAcc r y) -> MyAcc r x
+
+open Nat
+
+theorem div_lemma {x y : Nat} : 0 < y ∧ y ≤ x → x - y < x :=
+  fun h => sub_lt (Nat.lt_of_lt_of_le h.left h.right) h.left
+
+def div.F (x : Nat) (f : (x₁ : Nat) → x₁ < x → Nat → Nat) (y : Nat) : Nat :=
+  if h : 0 < y ∧ y ≤ x then
+    f (x - y) (div_lemma h) y + 1
+  else
+    zero
+
+-- noncomputable def div := WellFounded.fix (measure id).wf div.F
+noncomputable def div := WellFounded.fix (Nat.lt_wfRel.wf) div.F
+
+
+-- def div (x y : Nat) : Nat :=
+--   if h : 0 < y ∧ y ≤ x then
+--     have : x - y < x := Nat.sub_lt (Nat.lt_of_lt_of_le h.1 h.2) h.1
+--     (div (x - y) y) + 1
+--   else
+--     0
+
+-- #eval div 8 2
+
+lemma div_thing (x y z) : div x y = z → x ≠ 3 := by
+intro d
+induction d;
+sorry
+
+
+lemma h : ∀ n : Nat, n < .succ (.succ n) := sorry
+
+def is_even.F (x : Nat) (f : (x' : Nat) -> x' < x -> Bool) : Bool :=
+match x with
+| .zero => true
+| .succ .zero  => false
+| .succ (.succ n)  =>
+    f n (h n)
+
+noncomputable def is_even := WellFounded.fix (Nat.lt_wfRel.wf) is_even.F
+
+lemma is_even_thing (n z) : is_even n = z → n ≠ 3 := by
+induction n
+case zero => sorry;
+case succ n ih => sorry;
+
+
+#check WellFounded.fix
+
+inductive Even.F : ∀ x : Nat, (∀ x' : Nat, x' < x -> Prop) → Prop
+| z P : Even.F Nat.zero P
+| ss n P: P n (h n) -> Even.F (.succ (.succ n)) P
+
+#check Nat.lt_wfRel
+#check Nat.lt_wfRel.wf
+#check id
+#check measure id
+#check WellFoundedRelation
+#check WellFounded.fix
+noncomputable def Even := WellFounded.fix Nat.lt_wfRel.wf Even.F
+
+lemma even_thing {n} : Even n → n ≠ 3 := by
+induction n;
+case zero => sorry;
+case succ n ih => sorry;
