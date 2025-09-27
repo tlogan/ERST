2807e2e1d6ad735a74054cea66352acc76bfe326
Thomas Logan <1215556+tlogan@users.noreply.github.com>
2025-09-27 14:18:37 -0500

    update diff lemmas

--- dsn/Lang/Generic.lean
+++ dsn/Lang/Generic.lean
@@ -3,6 +3,10 @@ import Lang.Basic
 import Lang.Static
 import Lang.Dynamic
 
+
+-- import Std.Data.List.Basic
+-- import Std.Data.List.Lemmas
+
 set_option pp.fieldNotation false
 
 
@@ -21,37 +25,135 @@ lemma erase_introduction (xs0 : List String) :
   ∀ xs1, xs0 ⊆ xs1 → ∀ x, x ∉ xs0 → x ∈ xs1 → xs0 ⊆ (List.erase xs1 x)
 := by sorry
 
-lemma diff_preservation (xs : List String) :
-  ∀ xs0 xs1, xs0 ⊆ xs1 → List.diff xs0 xs ⊆ List.diff xs1 xs
+lemma diff_increasing_preservation (xs : List String) :
+  ∀ ys zs, ys ⊆ zs → List.diff ys xs ⊆ List.diff zs xs
 := by
   induction xs with
   | nil =>
     simp [List.diff]
   | cons x xs ih =>
-    intro xs0 xs1 ss
+    intro ys zs ss
     -- apply ih at ss
     simp [List.diff]
-    have d0 : Decidable (x ∈ xs0) := inferInstance
+    have d0 : Decidable (x ∈ ys) := inferInstance
     cases d0 with
     | isFalse h0 =>
-      have d1 : Decidable (x ∈ xs1) := inferInstance
+      have d1 : Decidable (x ∈ zs) := inferInstance
       cases d1 with
       | isFalse h1 => simp [*]
       | isTrue h1 =>
         simp [*]
-        have h2 : xs0 ⊆ (List.erase xs1 x) := by
-          exact erase_introduction xs0 xs1 ss x h0 h1
+        have h2 : ys ⊆ (List.erase zs x) := by
+          exact erase_introduction ys zs ss x h0 h1
         simp [*]
     | isTrue h0 =>
       simp [*]
-      have h1 : x ∈ xs1 := by apply ss; simp [*]
+      have h1 : x ∈ zs := by apply ss; simp [*]
       simp [*]
       apply ih
-      exact erase_preservation xs0 xs1 ss x h0 (ss h0)
+      exact erase_preservation ys zs ss x h0 (ss h0)
 
-lemma ListPair.mem_concat_disj {β}
-  (x : String) (am1 am0 : List (String × β)): x ∈ ListPair.dom (am1 ++ am0)
+#check ListPair.dom
+
+
+-- lemma ListPair.mem_concat_disj {β}
+--   (x : String) (am1 am0 : List (String × β))
+-- :
+--   x ∈ ListPair.dom (am1 ++ am0) →
+--   x ∈ ListPair.dom am1 ∨ x ∈ ListPair.dom am0
+-- := by induction am1 with
+--   | nil =>
+--     simp [dom]
+--   | cons a am1' ih =>
+--     simp [dom]
+--     intro p
+--     cases p with
+--     | inl h =>
+--       simp [*]
+--     | inr h =>
+--       apply ih at h
+--       cases h with
+--       | inl h' =>
+--         simp [*]
+--       | inr h' =>
+--         simp [*]
+
+-- lemma ListPair.mem_disj_concat_left {β}
+--   (x : String) (am1 am0 : List (String × β))
+-- :
+--   x ∈ ListPair.dom am1 →
+--   x ∈ ListPair.dom (am1 ++ am0)
+-- := by induction am1 with
+--   | nil => simp [dom]
+--   | cons a am1' ih =>
+--     simp [dom]
+--     intro p
+--     cases p with
+--     | inl h =>
+--       simp [*]
+--     | inr h =>
+--       apply ih at h
+--       simp [*]
+
+-- lemma ListPair.mem_disj_concat_right {β}
+--   (x : String) (am1 am0 : List (String × β))
+-- :
+--   x ∈ ListPair.dom am0 →
+--   x ∈ ListPair.dom (am1 ++ am0)
+-- := by induction am1 with
+--   | nil => simp
+--   | cons a am1' ih =>
+--     simp [dom]
+--     intro p
+--     apply ih at p
+--     simp [*]
+
+
+
+lemma diff_concat_preservation (xs ys zs : List String) :
+  List.diff xs (ys ++ zs) ⊆ List.diff xs ys
 := by sorry
+
+
+lemma diff_erase_right_preservation (xs zs : List String) y :
+List.diff xs zs ⊆ List.diff xs (List.erase zs y)
+:= by sorry
+
+
+lemma diff_concat_diff_preservation (ys : List String) :
+∀ xs zs, ys ⊆ zs → List.diff xs zs ⊆ List.diff xs (ys ++ List.diff zs ys)
+:= by induction ys with
+  | nil =>
+    simp [List.diff]
+  | cons y ys' ih =>
+    simp [List.diff]
+    intro xs zs yzs ss x m
+    have dx : Decidable (y ∈ xs) := inferInstance
+    have dz : Decidable (y ∈ zs) := inferInstance
+    sorry
+
+
+lemma diff_decreasing_preservation (xs ys zs : List String) :
+  ys ⊆ zs → List.diff xs zs ⊆ List.diff xs ys
+:= by
+  intro ss
+  intro x dz
+  apply diff_concat_preservation xs ys (List.diff zs ys)
+  apply diff_concat_diff_preservation ys xs zs ss
+  simp [*]
+
+
+
+
+
+  -- ys ⊆ zs → List.diff xs zs ⊆ List.diff xs ys
+
+  -- List.diff xs zs ⊆ List.diff xs (ys ++ List.diff zs ys)
+  -- List.diff xs (ys ++ zs') ⊆ List.diff xs ys
+
+
+
+
 
 lemma
 dom_diff_concat {β} (am0 am1 : List (String × β))
@@ -68,7 +170,7 @@ ListPair.dom (am1 ++ am0) ⊆ List.diff xs' xs
   | nil =>
     simp [*]
     have ss1' : List.diff xs_im xs ⊆ List.diff xs' xs := by
-      apply diff_preservation; simp [*]
+      apply diff_increasing_preservation; simp [*]
     intro x p
     apply ss1'
     apply dd0
@@ -78,21 +180,15 @@ ListPair.dom (am1 ++ am0) ⊆ List.diff xs' xs
     simp [ListPair.dom]
     apply And.intro
     case left =>
-      have ss2 : xs ⊆ xs' := by
-        intro x p
-        apply ss1
-        apply ss0
-        apply p
-      have h : ListPair.dom am1' ⊆ List.diff xs' xs_im := by
-        intro x p
-        apply dd1
-        simp [ListPair.dom]
-        simp [*]
-      apply ih h
-      apply ListPair.mem_concat_disj
+      simp [ListPair.dom] at dd1
+      apply diff_decreasing_preservation xs' xs xs_im ss0
+      simp [*]
     case right =>
-
-      sorry
+      simp [ListPair.dom] at dd1
+      cases dd1 with | intro dd1l dd1r =>
+      intro x h
+      apply ih dd1r
+      simp [*]
 
 #print List.concat
 
